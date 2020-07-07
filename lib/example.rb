db9408b856c018f0be6c7f865d689ced7fb47318
Omayma Mahjoub <omayma.mahjoub@ensi-uma.tn>
2020-07-07 08:36:13 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -2,13 +2,15 @@
 def say_hello_world_ten_times
   phrase = "Hello World!"
   puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
 end
+
+say_hello_world_ten_times()
+say_hello_world_ten_times()
+say_hello_world_ten_times()
+say_hello_world_ten_times()
+say_hello_world_ten_times()
+say_hello_world_ten_times()
+say_hello_world_ten_times()
+say_hello_world_ten_times()
+say_hello_world_ten_times()
+say_hello_world_ten_times()
